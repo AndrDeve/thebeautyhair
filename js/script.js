7f0002b1ef5874bea4de767226162e328e57bea9
AndyR <marlon_rivera95@hotmail.com>
2022-05-12 08:28:02 -0500

    primera subida3

--- js/script.js
+++ js/script.js
@@ -27,33 +27,3 @@ var swiper = new Swiper(".home-slider", {
       clickable: true,
     },
   });
-
-
-
-
-
-  var swiper = new Swiper(".blogs-slider", {
-    spaceBetween: 50,
-    grabCursor:true,
-    loop:true,
-    autoplay: {
-      delay: 3000,
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    breakpoints:{
-      0:{
-        slidesPerView:1,
-      },
-      768:{
-        slidesPerView:2,
-      },
-      991:{
-        slidesPerView:3,
-      },
-    },
-  });
-      
